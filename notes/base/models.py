d2d0249f7810538f204e704f0f45533d6840ee87
Sandesh <sdhakal712@gmail.com>
2025-03-20 15:35:23 +0545

    First migration to the db

--- notes/base/models.py
+++ notes/base/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class NoteCategory(models.Model):
+    name = models.CharField(max_length=100)
+
+class Note(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    category = models.ForeignKey(NoteCategory, on_delete=models.SET_NULL,null=True)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL,null=True)
+
+class gukha(models.Model):
+    name = models.CharField(max_length=200)
+    category = models.ForeignKey(Note,on_delete=models.SET_NULL,null=True)
--- notes/notes/settings.py
+++ notes/notes/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'base',
 ]
 
 MIDDLEWARE = [
@@ -73,13 +74,24 @@ WSGI_APPLICATION = 'notes.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+DATABASES = {
+    'default':{
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'notes',
+        'USER': 'postgres',
+        'PASSWORD':'password',
+        'HOST': 'localhost',
+        'PORT': '5432', # default PostgreSQL port
+        }
+}
+    
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
